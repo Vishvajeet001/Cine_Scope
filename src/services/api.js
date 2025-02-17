9d05379a00296cc17f85e4f6ad2a82762728bf4e
Vishvajeet001 <vishvajeetdeswal93056@gmail.com>
2025-02-17 18:49:15 +0530

    api key change

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,4 @@
-// const rapidApiKey = "d030507913msh0f32368a40772e4p166eddjsn79426fb14b47"; used
-const rapidApiKey = "f7ecb3f80emsh53660291bd62e23p15ab1fjsn78402de6a945";
-// const rapidApiKey = "d8bbccf172msh744a6d844ea5a80p101835jsn08be4d125c45";
-// const rapidApiKey = "";
+const rapidApiKey = "d8bbccf172msh744a6d844ea5a80p101835jsn08be4d125c45";
 
 const options = {
   method: "GET",
